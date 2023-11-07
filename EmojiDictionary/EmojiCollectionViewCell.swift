329eeab2b97ef23b95b7e542965b081eade4d633
wayne tsao <a578ff6@gmail.com>
2023-11-08 05:03:41 +0800

    collectionView

---
+++ EmojiDictionary/EmojiCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiDictionary
+//
+//  Created by 曹家瑋 on 2023/11/8.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var symbolLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+}
